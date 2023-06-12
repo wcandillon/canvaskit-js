a2d619787f84de815e14087c037d533eea87c2dc
William Candillon <wcandillon@gmail.com>
2023-06-12 06:26:20 +0200

    :wrench:

--- example/src/Shaders.tsx
+++ example/src/Shaders.tsx
@@ -9,7 +9,7 @@ import {
   useOnDraw,
   useImage,
 } from "./components";
-import zurich from "./assets/zurich2.jpg";
+import zurich from "./assets/zurich.jpg";
 
 const filter = CanvasKit.RuntimeEffect.Make(`precision mediump float;
 
@@ -17,16 +17,12 @@ uniform sampler2D child;
 uniform vec2 resolution;
 uniform float r;
 
-
 void main() {
   vec2 xy = gl_FragCoord.xy;
   xy.x += sin(xy.y / r) * 4.0;
   xy /= resolution;
   gl_FragColor = texture2D(child, vec2(xy.x, 1.0 - xy.y)).rbga;
 }`)!;
-
-// TODO: remove
-let imageShader: null | Shader = null;
 
 const drawShader = (
   image: Image | null,
@@ -40,18 +36,15 @@ const drawShader = (
   const input = CanvasKit.XYWHRect(0, 0, image.width(), image.height());
   const output = CanvasKit.XYWHRect(0, 0, width, height);
   const transform = fitbox("cover", input, output);
-  if (!imageShader) {
-    imageShader = image.makeShaderOptions(
-      CanvasKit.TileMode.Clamp,
-      CanvasKit.TileMode.Clamp,
-      CanvasKit.FilterMode.Linear,
-      CanvasKit.MipmapMode.None,
-      transform
-    );
-  }
+  const imageShader = image.makeShaderOptions(
+    CanvasKit.TileMode.Clamp,
+    CanvasKit.TileMode.Clamp,
+    CanvasKit.FilterMode.Linear,
+    CanvasKit.MipmapMode.None,
+    transform
+  );
 
   const paint = new CanvasKit.Paint();
-  //paint.setShader(imageShader!);
   paint.setShader(
     filter.makeShaderWithChildren(
       [width, height, mix(progress.value, 1, 100)],
