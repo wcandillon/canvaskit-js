82ff4d4e6359734ac2d42daa174441daf9154fa1
William Candillon <wcandillon@gmail.com>
2023-07-04 10:25:03 +0200

    :wrench:

--- package/src/__tests__/Paths/PathTrim.test.ts
+++ package/src/__tests__/Paths/PathTrim.test.ts
@@ -3,17 +3,25 @@ import { TrimPathEffect } from "../../Path/PathEffects";
 import "../setup";
 
 const testTriming = (d: string) => {
-  const ts = [0, 0.25, 0.33, 0.5, 0.66, 0.75];
+  const ts = [0.25, 0.33, 0.5, 0.66, 0.75];
   const pathRef = RealCanvasKit.Path.MakeFromSVGString(d)!;
   const parsed = parseSVG(d)!;
   expect(parsed).toBeTruthy();
   const pathTest = parsed.getPath();
   expect(pathRef).toBeTruthy();
-  for (const t of ts) {
-    const path1 = pathRef.copy().trim(0, t, false)!;
-    const pe = new TrimPathEffect(0, t, false);
-    const path2 = pe.filterPath(pathTest);
-    expect(Array.from(path1.toCmds())).toEqual(path2.toCmds());
+  for (const complement of [false, true]) {
+    for (const t of ts) {
+      const path1 = pathRef.copy().trim(0, t, complement)!;
+      const pe = new TrimPathEffect(0, t, complement);
+      const path2 = pe.filterPath(pathTest);
+      try {
+        expect(Array.from(path1.toCmds())).toEqual(path2.toCmds());
+      } catch (error) {
+        throw new Error(
+          `Failed at t = ${t} with path d = ${d} (complement = ${complement})`
+        );
+      }
+    }
   }
 };
 
@@ -21,6 +29,8 @@ describe("Path Trim", () => {
   it("Trim lines", () => {
     testTriming("M0 0 L 100 100");
     testTriming("M0 0 L 1 1");
+    testTriming("M0 0 L 50 50 L 100 0");
+    // testTriming("M0 0 L 50 50 L 100 0 L 150 50 L 200 0");
   });
   //   it("Trim a quadratic curves", () => {
   //     testTriming("M 0 0 Q 0 100 100 100");
