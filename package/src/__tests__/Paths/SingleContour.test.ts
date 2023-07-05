1aaf33023cdacf21d97242b7a72e5e292db21adc
William Candillon <wcandillon@gmail.com>
2023-07-05 15:19:45 +0200

    :wrench:

--- package/src/__tests__/Paths/SingleContour.test.ts
+++ package/src/__tests__/Paths/SingleContour.test.ts
@@ -18,15 +18,21 @@ beforeAll(() => {
 });
 
 describe("Single contour values", () => {
-  test.each(paths)("Check path length: %s", (d) => {
+  test.each(paths)("%s: getTotalLength()", (d) => {
     const [reference, test] = singleContours[d];
     const length = reference.getTotalLength();
     expect(length).toBeApproximatelyEqual(test.getTotalLength(), 0.3);
   });
-  test.each(paths)("Check posTan(0.5) for %s", (d) => {
+  const posTans = paths.flatMap((d) => [
+    [d, 0.3],
+    [d, 0.5],
+    [d, 0.7],
+  ]) as [string, number][];
+  test.each(posTans)("%s: posTan(%d)", (d, t) => {
     const [reference, test] = singleContours[d];
-    const posTan1 = reference.getPosTan(0.5);
-    const posTan2 = test.getPosTan(0.5);
+    const length = reference.getTotalLength();
+    const posTan1 = reference.getPosTan(t * length);
+    const posTan2 = test.getPosTan(t * length);
     expect(posTan1).toBeApproximatelyEqual(posTan2, 1);
   });
 });
