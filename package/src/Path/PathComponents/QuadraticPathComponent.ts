71345f8ecd147e8b2e624fbc293d20a40b4b8945
William Candillon <wcandillon@gmail.com>
2023-07-05 14:58:46 +0200

    :wrench:

--- package/src/Path/PathComponents/QuadraticPathComponent.ts
+++ package/src/Path/PathComponents/QuadraticPathComponent.ts
@@ -65,28 +65,38 @@ const quadraticSolve = (t: number, p0: number, p1: number, p2: number) => {
   );
 };
 
+//export const getQuadraticArcLength = (p1: Point, cp: Point, p2: Point) => {
 export const getQuadraticArcLength = (p1: Point, cp: Point, p2: Point) => {
-  const ax = p1[0] - 2 * cp[0] + p2[0];
-  const ay = p1[1] - 2 * cp[1] + p2[0];
-  const bx = 2 * cp[0] - 2 * p1[0];
-  const by = 2 * cp[1] - 2 * p1[1];
+  const xs = [p1[0], cp[0], p2[0]];
+  const ys = [p1[1], cp[1], p2[1]];
+  /*
+    xs: number[],
+  ys: number[],
+  */
+  const t = 1;
+  const ax = xs[0] - 2 * xs[1] + xs[2];
+  const ay = ys[0] - 2 * ys[1] + ys[2];
+  const bx = 2 * xs[1] - 2 * xs[0];
+  const by = 2 * ys[1] - 2 * ys[0];
 
   const A = 4 * (ax * ax + ay * ay);
   const B = 4 * (ax * bx + ay * by);
   const C = bx * bx + by * by;
 
   if (A === 0) {
-    return Math.sqrt(Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2));
+    return (
+      t * Math.sqrt(Math.pow(xs[2] - xs[0], 2) + Math.pow(ys[2] - ys[0], 2))
+    );
   }
   const b = B / (2 * A);
   const c = C / A;
-  const u = 1 + b;
+  const u = t + b;
   const k = c - b * b;
 
   const uuk = u * u + k > 0 ? Math.sqrt(u * u + k) : 0;
   const bbk = b * b + k > 0 ? Math.sqrt(b * b + k) : 0;
   const term =
-    b + Math.sqrt(b * b + k) !== 0 && (u + uuk) / (b + bbk) !== 0
+    b + Math.sqrt(b * b + k) !== 0 && (u + uuk) / (b + bbk) != 0
       ? k * Math.log(Math.abs((u + uuk) / (b + bbk)))
       : 0;
 
--- package/src/__tests__/Paths/SingleContour.test.ts
+++ package/src/__tests__/Paths/SingleContour.test.ts
@@ -5,8 +5,8 @@ const paths = [
   "M0 0 L200 200",
   "M 640.48 1285.21 Q 642.39 644.73 642.39 644.73",
   "M0 0 Q 0 200 200 200",
-  //"M 267 0 Q 382 0 512 0",
-  //"M 267 0 Q 383 265 512 0",
+  "M 267 0 Q 382 0 512 0",
+  "M 267 0 Q 383 265 512 0",
   "M200 200 C 275 100 575 100 500 200",
   "M0 0 C 0 200 0 200 200 200",
 ];
@@ -21,6 +21,6 @@ describe("Single contour values", () => {
   test.each(paths)("Check path length: %s", (d) => {
     const [reference, test] = singleContours[d];
     const length = reference.getTotalLength();
-    expect(length).toBeApproximatelyEqual(test.getTotalLength(), 3);
+    expect(length).toBeApproximatelyEqual(test.getTotalLength(), 0.3);
   });
 });
