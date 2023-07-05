5b4237c7db3ba98c8059ef527f143628cec6b14e
William Candillon <wcandillon@gmail.com>
2023-07-05 19:31:48 +0200

    :wrench:

--- package/src/Path/PathComponents/LinearPathComponent.ts
+++ package/src/Path/PathComponents/LinearPathComponent.ts
@@ -41,10 +41,5 @@ export class LinearPathComponent implements PathComponent {
   }
 }
 
-export const linearSolve = (t: number, p0: number, p1: number) => {
-  return (1 - t) * p0 + t * p1;
-};
-
-export const linearSolve2 = (t: number, p0: Point, p1: Point) => {
-  return vec(linearSolve(t, p0[0], p1[0]), linearSolve(t, p0[1], p1[1]));
-};
+export const linearSolve2 = (t: number, p0: Point, p1: Point) =>
+  vec((1 - t) * p0[0] + t * p1[0], (1 - t) * p0[1] + t * p1[1]);
