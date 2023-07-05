77b9150c7ef2daf4d364c96a3d48e0e3d947b4b3
William Candillon <wcandillon@gmail.com>
2023-07-05 23:06:51 +0200

    :wrench:

--- package/src/__tests__/Paths/setup.ts
+++ package/src/__tests__/Paths/setup.ts
@@ -1,4 +1,4 @@
-import type { ContourMeasure } from "canvaskit-wasm";
+import type { ContourMeasure, ContourMeasureIter, Path } from "canvaskit-wasm";
 
 export const prepareSingleContourTest = (
   d: string
@@ -12,3 +12,48 @@ export const prepareSingleContourTest = (
 
 export const singleContours: Record<string, [ContourMeasure, ContourMeasure]> =
   {};
+
+class ContoursMeasure {
+  private len = 0;
+
+  constructor(it: ContourMeasureIter) {
+    let c: ContourMeasure | null = null;
+    while ((c = it.next())) {
+      this.len += c.length();
+    }
+  }
+
+  getPosTan(
+    _distance: number,
+    _output?: Float32Array | undefined
+  ): Float32Array {
+    throw new Error("Method not implemented.");
+  }
+
+  getSegment(_startD: number, _stopD: number, _startWithMoveTo: boolean): Path {
+    throw new Error("Method not implemented.");
+  }
+
+  isClosed() {
+    return false;
+  }
+
+  length(): number {
+    return this.len;
+  }
+}
+
+export const multipleContours: Record<
+  string,
+  [ContoursMeasure, ContoursMeasure]
+> = {};
+
+export const prepareMultipleContourTest = (
+  d: string
+): [ContoursMeasure, ContoursMeasure] => {
+  const pathRef = RealCanvasKit.Path.MakeFromSVGString(d)!;
+  const path = CanvasKit.Path.MakeFromSVGString(d)!;
+  const contourItRef = new RealCanvasKit.ContourMeasureIter(pathRef, false, 1);
+  const contourIt = new CanvasKit.ContourMeasureIter(path, false, 1);
+  return [new ContoursMeasure(contourItRef), new ContoursMeasure(contourIt)];
+};
