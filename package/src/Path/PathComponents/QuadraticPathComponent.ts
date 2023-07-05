1a901c50f6ff808669fc350f464b98844ce8409e
William Candillon <wcandillon@gmail.com>
2023-07-05 21:35:35 +0200

    :wrench:

--- package/src/Path/PathComponents/QuadraticPathComponent.ts
+++ package/src/Path/PathComponents/QuadraticPathComponent.ts
@@ -1,4 +1,5 @@
 import type { Point } from "canvaskit-wasm";
+import { svgPathProperties } from "svg-path-properties";
 
 import { vec } from "../../Vector";
 import { PathVerb } from "../../Core";
@@ -7,7 +8,13 @@ import type { PathComponent } from "./PathComponent";
 import { linearSolve } from "./LinearPathComponent";
 
 export class QuadraticPathComponent implements PathComponent {
-  constructor(readonly p1: Point, readonly cp: Point, readonly p2: Point) {}
+  props: ReturnType<typeof svgPathProperties>;
+
+  constructor(readonly p1: Point, readonly cp: Point, readonly p2: Point) {
+    this.props = new svgPathProperties(
+      `M${p1[0]} ${p1[1]} Q${cp[0]} ${cp[1]} ${p2[0]} ${p2[1]}`
+    );
+  }
 
   getSegment(t0: number, t1: number) {
     // First cut at t0
@@ -28,9 +35,7 @@ export class QuadraticPathComponent implements PathComponent {
   }
 
   length() {
-    const xs = [this.p1[0], this.cp[0], this.p2[0]];
-    const ys = [this.p1[1], this.cp[1], this.p2[1]];
-    return getQuadraticArcLength(xs, ys);
+    return this.props.getTotalLength();
   }
 
   toSVGString() {
@@ -41,99 +46,13 @@ export class QuadraticPathComponent implements PathComponent {
     return [PathVerb.Quad, this.cp[0], this.cp[1], this.p2[0], this.p2[1]];
   }
 
-  getPointAtLength(length: number): Point {
-    const xs = [this.p1[0], this.cp[0], this.p2[0]];
-    const ys = [this.p1[1], this.cp[1], this.p2[1]];
-    const t = t2length(length, this.length(), (i) =>
-      getQuadraticArcLength(xs, ys, i)
-    );
-    const x =
-      (1 - t) * (1 - t) * (1 - t) * xs[0] +
-      3 * (1 - t) * (1 - t) * t * xs[1] +
-      3 * (1 - t) * t * t * xs[2] +
-      t * t * t * xs[3];
-    const y =
-      (1 - t) * (1 - t) * (1 - t) * ys[0] +
-      3 * (1 - t) * (1 - t) * t * ys[1] +
-      3 * (1 - t) * t * t * ys[2] +
-      t * t * t * ys[3];
+  getPointAtLength(length: number) {
+    const { x, y } = this.props.getPointAtLength(length);
     return vec(x, y);
   }
 
-  getTangentAtLength(length: number): Point {
-    const t = length / this.length();
-    const dx =
-      2 * ((1 - t) * (this.cp[0] - this.p1[0]) + t * (this.p2[0] - this.cp[0]));
-    const dy =
-      2 * ((1 - t) * (this.cp[1] - this.p1[1]) + t * (this.p2[1] - this.cp[1]));
-
-    const magnitude = Math.hypot(dx, dy);
-    return vec(dx / magnitude, dy / magnitude);
+  getTangentAtLength(length: number) {
+    const { x, y } = this.props.getTangentAtLength(length);
+    return vec(x, y);
   }
 }
-
-const getQuadraticArcLength = (xs: number[], ys: number[], t = 1) => {
-  const ax = xs[0] - 2 * xs[1] + xs[2];
-  const ay = ys[0] - 2 * ys[1] + ys[2];
-  const bx = 2 * xs[1] - 2 * xs[0];
-  const by = 2 * ys[1] - 2 * ys[0];
-
-  const A = 4 * (ax * ax + ay * ay);
-  const B = 4 * (ax * bx + ay * by);
-  const C = bx * bx + by * by;
-
-  if (A === 0) {
-    return (
-      t * Math.sqrt(Math.pow(xs[2] - xs[0], 2) + Math.pow(ys[2] - ys[0], 2))
-    );
-  }
-  const b = B / (2 * A);
-  const c = C / A;
-  const u = t + b;
-  const k = c - b * b;
-
-  const uuk = u * u + k > 0 ? Math.sqrt(u * u + k) : 0;
-  const bbk = b * b + k > 0 ? Math.sqrt(b * b + k) : 0;
-  const term =
-    b + Math.sqrt(b * b + k) !== 0 && (u + uuk) / (b + bbk) !== 0
-      ? k * Math.log(Math.abs((u + uuk) / (b + bbk)))
-      : 0;
-
-  return (Math.sqrt(A) / 2) * (u * uuk - b * bbk + term);
-};
-
-export const t2length = (
-  length: number,
-  totalLength: number,
-  func: (t: number) => number
-) => {
-  let error = 1;
-  let t = length / totalLength;
-  let step = (length - func(t)) / totalLength;
-
-  let numIterations = 0;
-  while (error > 0.001) {
-    const increasedTLength = func(t + step);
-    const increasedTError = Math.abs(length - increasedTLength) / totalLength;
-    if (increasedTError < error) {
-      error = increasedTError;
-      t += step;
-    } else {
-      const decreasedTLength = func(t - step);
-      const decreasedTError = Math.abs(length - decreasedTLength) / totalLength;
-      if (decreasedTError < error) {
-        error = decreasedTError;
-        t -= step;
-      } else {
-        step /= 2;
-      }
-    }
-
-    numIterations++;
-    if (numIterations > 500) {
-      break;
-    }
-  }
-
-  return t;
-};
