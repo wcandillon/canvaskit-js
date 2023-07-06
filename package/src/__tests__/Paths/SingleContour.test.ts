238a1ed786c0c6ee16e1aaf2ce823983cde9a0a5
William Candillon <wcandillon@gmail.com>
2023-07-06 14:16:00 +0200

    :wrench:

--- package/src/__tests__/Paths/SingleContour.test.ts
+++ package/src/__tests__/Paths/SingleContour.test.ts
@@ -29,9 +29,10 @@ describe("Single contour values", () => {
     expect(length).toBeApproximatelyEqual(test.length(), 1);
   });
   const dt = paths.flatMap((d) => [
-    [d, 0.3],
+    [d, 0.1],
+    [d, 0.33],
     [d, 0.5],
-    [d, 0.7],
+    [d, 0.66],
   ]) as [string, number][];
   test.each(dt)("%s: posTan(%d)", (d, t) => {
     const [reference, test] = singleContours[d];
@@ -40,4 +41,18 @@ describe("Single contour values", () => {
     const posTan = test.getPosTan(length);
     expect(posTanRef).toBeApproximatelyEqual(posTan, 1);
   });
+  // const ranges = paths.flatMap((d) => [
+  //   [d, 0, 0.25],
+  //   [d, 0.3, 0.5],
+  //   [d, 0.5, 1],
+  //   [d, 0, 0.5],
+  // ]) as [string, number, number][];
+  // test.each(ranges)("%s: trim(%d, %d)", (d, t0, t1) => {
+  //   const [reference, test] = singleContours[d];
+  //   const start = t0 * reference.length();
+  //   const end = t1 * reference.length();
+  //   const pathRef = reference.getSegment(start, end, true);
+  //   const path = test.getSegment(start, end, true);
+  //   expect(pathRef.toCmds()).toBeApproximatelyEqual(path.toCmds(), 1);
+  // });
 });
